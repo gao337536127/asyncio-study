a4cbff339f2cba457225ebad189350e7945185eb
高文俊 <gwj337536127@163.com>
2026-04-05 17:35:42 +0800

    第四章

---
+++ 4-1.py
@@ -0,0 +1,46 @@
+import asyncio
+import socket
+from types import TracebackType
+from typing import Optional, Type
+
+
+class ConnectedSocket:
+    def __init__(self, server_socket):
+        self.connection = None
+        self._server_socket = server_socket
+
+    async def __aenter__(self):
+        print(f"Entering context manager, waiting for connection")
+        loop = asyncio.get_event_loop()
+        connection, address = await loop.sock_accept(self._server_socket)
+        self._connection = connection
+        print(f"Accepted a connection")
+        return self._connection
+
+    async def __aexit__(
+        self,
+        exc_type: Optional[TracebackType],
+        exc: Optional[BaseException],
+        tb: Optional[TracebackType],
+    ):
+        print("Exiting context manager")
+        self._connection.close()
+        print(f"Closed connection")
+
+
+async def main():
+    loop = asyncio.get_event_loop()
+
+    server_socket = socket.socket()
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_address = ("127.0.0.1", 8000)
+    server_socket.setblocking(False)
+    server_socket.bind(server_address)
+    server_socket.listen()
+
+    async with ConnectedSocket(server_socket) as connection:
+        data = await loop.sock_recv(connection, 1024)
+        print(data)
+
+
+asyncio.run(main())
